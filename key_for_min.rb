c8583a18cdc9b9e20afd71866c94abe5d28c24be
Calie Rushton <calielrushton@gmail.com>
2019-01-07 09:27:26 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,13 @@ def key_for_min_value(name_hash)
     if price_of_smallest == nil || price < price_of_smallest
       #price_of_smallest is == nil on first iteration.  Gives something to compare to on subsequent iterations
       price_of_smallest = price
+      # if current iteration has smaller value, replace value and item name...
       name_of_smallest = item
     end
   end
   name_of_smallest
 end
+
+
+#Another user posted the exact problem I was having here....
+#https://github.com/learn-co-curriculum/key-for-min-value/commit/5ef8507158fa6d6a1b8499b494bc9d418c8d055c
