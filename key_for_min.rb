934e8ca80b6a6f53a4b844859fed08364e7dd378
Calie Rushton <calielrushton@gmail.com>
2019-01-07 09:26:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_of_smallest = nil
+  price_of_smallest = nil
+  # need a value variable declared but not set to anything, to add first value to on first iteration...
+  name_hash.each do |item, price|
+    if price_of_smallest == nil || price < price_of_smallest
+      #price_of_smallest is == nil on first iteration.  Gives something to compare to on subsequent iterations
+      price_of_smallest = price
+      name_of_smallest = item
+    end
+  end
+  name_of_smallest
 end
