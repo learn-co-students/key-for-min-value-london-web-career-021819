36404520f8fdd40e0ef9df3c9a96e8973e051b58
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-06 15:51:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,30 @@
 
 def key_for_min_value(name_hash)
 
+i = 10000
+   
+  min_key = {}
+  
+  if name_hash.length > 0
+    
+    name_hash.each do |key, value|
+      
+      if value < i
+        
+        min_key = key
+        
+        i = value
+        
+      end
+      
+    end
+    
+    min_key
+    
+else
+    
+    nil
+    
+  end
+    
 end
